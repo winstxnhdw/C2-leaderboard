92e04bbaa63853c45b218e19a0b256149dc83a73
winstxnhdw <winstxnhdw@gmail.com>
2022-05-26 05:11:19 +0800

    full reset docker on every launch

--- launch.bat
+++ launch.bat
@@ -2,9 +2,10 @@
 
 set app_name=leaderboard-app
 
-docker-compose down
+docker-compose rm -f -s -v
 
 docker load --input bin\postgres-alpine.tar
 docker load --input bin\leaderboard-app.tar
 
 docker-compose up -d
+docker-compose logs -f -t
